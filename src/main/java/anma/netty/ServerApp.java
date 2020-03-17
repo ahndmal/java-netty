7bca1f360c4d55a86c1daba3194cb787f9481981
Andrii Maliuta <quadr988@gmail.com>
2020-03-17 19:33:40 +0200

    init

---
+++ src/main/java/anma/netty/ServerApp.java
@@ -0,0 +1,45 @@
+package anma.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class ServerApp {
+
+    private static final int PORT = 8189;
+
+    public static void main(String[] args) {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(1);
+
+        try {
+            ServerBootstrap serverBootstrap= new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast( new SimpleHandler());
+                        }
+                    });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(PORT).sync();
+
+            System.out.println("Server started");
+
+            channelFuture.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/anma/netty/SimpleHandler.java
@@ -0,0 +1,33 @@
+package anma.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.ByteBuffer;
+
+public class SimpleHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("New client connected");
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        ByteBuf buf = (ByteBuf) msg;
+
+        while (buf.readableBytes() > 0) {
+            System.out.println((char)buf.readByte());
+        }
+        buf.release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+        ctx.close();
+        cause.printStackTrace();
+    }
+}
